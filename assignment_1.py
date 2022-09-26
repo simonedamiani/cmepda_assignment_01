bb1360d9e1b050f837596dd314919ba6a932c627
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-09-27 00:22:51 +0200

    Initial commit

---
+++ assignment_1.py
@@ -0,0 +1,18 @@
+import argparse
+
+def process(file_path):
+    """
+    """
+    print(f'Opening input file {file_path}...')
+    with open(file_path, 'r') as input_file:
+        text = input_file.read()
+    print(text)
+    print('Done.')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Print some book statistics')
+    parser.add_argument('infile', type=str, help='path to the input file')
+    args = parser.parse_args()
+    process(args.infile)
