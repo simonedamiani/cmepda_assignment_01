6909ef5ff377af6aa844da35ab476fbdabb0c760
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-09-28 12:07:08 +0200

    Initial commit

--- assignment_1.py
+++ assignment_1.py
@@ -1,18 +1,16 @@
 import argparse
 
 def process(file_path):
-    """
-    """
+    """Open file located in 'file_path' and convert it to string"""
     print(f'Opening input file {file_path}...')
     with open(file_path, 'r') as input_file:
         text = input_file.read()
-    print(text)
     print('Done.')
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Print some book statistics')
-    parser.add_argument('infile', type=str, help='path to the input file')
+    parser.add_argument('infile', type=str, help='Path to the input file')
     args = parser.parse_args()
     process(args.infile)
