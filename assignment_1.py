64704ad615f1d8d326760f0b88cfaa7995ed974c
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-09-29 15:08:16 +0200

    Initial commit

--- assignment_1.py
+++ assignment_1.py
@@ -2,34 +2,38 @@ import argparse
 import collections
 import time
 import matplotlib.pyplot as plt
+import logging
 
 
 def process(file_path):
     """Open file located in 'file_path' and convert it to string"""
-    print(f'Opening input file {file_path}...')
+    logging.info(f' Opening input file {file_path}...')
     with open(file_path, 'r') as input_file:
         book = input_file.read()
-    print('Done.')
+    logging.info(' Done.')
     return book
 
 
 def alphabetical(raw_text):
     """Convert a text in an only alphabetical string"""
-    print('Removing all non-alphabetical characters from text...')
+    logging.info(' Removing all non-alphabetical characters from text...')
     plain_text = ''
     for all_char in raw_text:
         if all_char.isalpha():
             plain_text += all_char
-    print('Done.')
+    logging.info(' Done.')
     return plain_text
 
 
 def histogram(dict_):
+    """Create a histogram from data contained in a dict(), showing keys on x-axis"""
     plt.bar(list(dict_.keys()), dict_.values(), color='b')
     plt.show()
 
 
 if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO)
 
     start = time.time()
 
@@ -51,12 +55,14 @@ if __name__ == '__main__':
     for key in freq:
         print(key, '=', round(freq[key]*100, 2), '%')
 
-    print('Display a histogram to summarize the results?')
+    logging.warning(' Display a histogram to summarize the results?')
     response = input('Input Y/N:').lower()
     if response == 'y':
         histogram(freq)
     elif response not in 'yn':
-        print('Input character not recognized')
+        logging.error(' Input character not recognized')
 
     end = time.time()
-    print('Measured elapsed time:', round(end-start, 2), 'sec')
+    time_ = round(end-start, 2)
+
+    logging.info(f' Measured elapsed time: {time_}sec')
